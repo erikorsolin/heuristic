dee5e0b78510ba2ad649d4dfca4f0655dc703801
Erik Orsolin <depaulaerik670@gmail.com>
2023-02-09 11:03:28 -0300

    definição de tipos e criação de métodos

--- empacotamento.go
+++ empacotamento.go
@@ -11,8 +11,38 @@ type caixa struct{
 	largura float64
 	altura float64
 	profundidade float64
+	items []*item // items que foram empacotados 
 
-	items []*item // items que foram empacotados
+}
 
+
+// funcao que cria uma nova caixa com os parâmetros passados
+func NovaCaixa(largura, altura, profundidade float64) *caixa {
+		return &caixa{  		// o operador & serve para retornar o endereço de memória dessa variável
+			largura: largura,
+			altura: altura,
+			profundidade: profundidade,
+			items: make([]*item, 0),    
+
+		}
+
+}
+
+
+// Gets
+func (l *caixa) GetLargura() float64{
+	return l.largura
+}
+
+func (a *caixa) GetAltura() float64{
+	return a.altura
+}
+
+func (p *caixa) GetProfundidade() float64{
+	return p.profundidade
+}
+
+func (x *caixa) GetVolume() float64{
+	return x.altura * x.largura * x.profundidade
 }
 
